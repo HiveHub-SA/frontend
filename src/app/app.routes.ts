2f2d71adaa5fc1aa4213a1fabc0c4bd1217804d8
Franco Goti <gotifranco@gmail.com>
2026-07-23 19:19:56 -0300

    fix: modificaciones para mergear en dev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,26 @@
 import { Routes } from '@angular/router';
+import { ApiarioListComponent} from './apiarios/list/list-apiarios';
+import { ApiarioDetailComponent} from './apiarios/detail/detalle-apiario';
+import { ColmenaDetailComponent} from './colmenas/detail/detalle-colmena';
+import { MapaInteractivo } from './mapa-interactivo/mapa-interactivo';
+import { App } from './app';
+import { Inicio } from './inicio/inicio';
 import { OperacionSalaComponent } from './operaciones_sala/operacion_sala.component';
-import { MapaComponent } from './mapa/mapa.component';
-import { ApiariosComponent } from './apiarios/apiarios.component';
+
 
 export const routes: Routes = [
-  { path: 'mapa', component: MapaComponent },
-  { path: 'apiarios', component: ApiariosComponent },
+  {path: "", component: MapaInteractivo},
+  {path: 'apiarios', component: ApiarioListComponent, },
+  {path: 'apiarios/:id', component: ApiarioDetailComponent, },
+  {path: 'colmenas/:id', component: ColmenaDetailComponent, },
+  {path: "mapa", component: MapaInteractivo},
+  {path: "inicio", component: Inicio},
   { path: 'extraccion', component: OperacionSalaComponent },
-  { path: '', redirectTo: 'extraccion', pathMatch: 'full' }
 ];
+
+
+
+
+
+
+
