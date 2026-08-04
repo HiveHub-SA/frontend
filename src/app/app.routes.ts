ab4d7b48840a54be4e169784333d6c9f52133942
JereAntunez <jeremias.antunez18@gmail.com>
2026-08-04 14:58:24 -0300

    feat: Add audio recorder component with IndexedDB support for saving audio notes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,17 @@ import { MapaInteractivo } from './mapa-interactivo/mapa-interactivo';
 import { App } from './app';
 import { Inicio } from './inicio/inicio';
 import { OperacionSalaComponent } from './operaciones_sala/operacion_sala.component';
+import { AudioRecorderComponent } from './audio-recorder/audio-recorder';
 
 
 export const routes: Routes = [
-  {path: "", component: MapaInteractivo},
-  {path: 'apiarios', component: ApiarioListComponent, },
-  {path: 'apiarios/:id', component: ApiarioDetailComponent, },
-  {path: 'colmenas/:id', component: ColmenaDetailComponent, },
-  {path: "mapa", component: MapaInteractivo},
-  {path: "inicio", component: Inicio},
+  { path: '', component: MapaInteractivo },
+  { path: 'apiarios', component: ApiarioListComponent },
+  { path: 'apiarios/:id', component: ApiarioDetailComponent },
+  { path: 'colmenas/:id', component: ColmenaDetailComponent },
+  { path: 'mapa', component: MapaInteractivo },
+  { path: 'inicio', component: Inicio },
   { path: 'extraccion', component: OperacionSalaComponent },
+  { path: 'voz', component: AudioRecorderComponent },
 ];
 
