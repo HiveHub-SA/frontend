bdf2c83580f0b2c6b3989ac3ee6982b21828eda7
JereAntunez <jeremias.antunez18@gmail.com>
2026-08-29 15:59:02 -0300

    feat: Add ChangeDetectorRef and OnDestroy imports to InspeccionarColmenaComponent

--- src/app/inspecciones/colmena/inspeccionar-colmena.ts
+++ src/app/inspecciones/colmena/inspeccionar-colmena.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
